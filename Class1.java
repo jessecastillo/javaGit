358c4b49046104bdc3489386e573974a7ac4e3f1
jessecastillo <6.24jcastillorolltide@gmail.com>
2022-01-24 13:09:34 -0600

    adding hotfix branch

--- Class1.java
+++ Class1.java
@@ -1,5 +1,12 @@
 
-public static int class1(){
+public class Class1{
+/* public static int class1(){
 
     System.out.println("Is that even possible");
+}
+*/
+
+    private void f3(){
+    // v3.1 (contains bugfix)
+    }
 }
